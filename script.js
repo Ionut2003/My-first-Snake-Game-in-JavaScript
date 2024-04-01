14dba507b4d76a58557bcb95e7544364cdee608b
Ionut Paraschiv <ionutparaschiv360@gmail.com>
2024-04-01 18:17:08 +0300

    big changes

--- script.js
+++ script.js
@@ -1,106 +1,211 @@
 let canvas = document.getElementById("theCanvas");
-let ctx = canvas.getContext("2d");
-let square = 15;
-let gameEnd = document.getElementById("background");
-let theRetry = document.getElementById("retry");
-let overGame = document.getElementById("gameOver");
-let apple = new Image();
-let snake = [];
+let speed = 2;
+let intervalTime = 10;
+let snake = document.getElementById("snake");
+let snakeSquares = document.getElementsByClassName("squares");
 let direction = "nodirection";
-let Score = 0;
+let score = 0;
 let highScore = 0;
-let theScore = document.querySelector(".score");
-
-canvas.height = canvas.width;
+let xPos = parseFloat(getComputedStyle(snake).getPropertyValue("left"));
+let yPos = parseFloat(getComputedStyle(snake).getPropertyValue("top"));
+const apple = document.getElementById("apple");
+const scoreText = document.getElementById("score");
+const highScoreText = document.getElementById("highScore");
+let positions = [];
+let pause = false;
 // DRAWING THE SNAKE BOARD
+apple.style.left = Math.floor(Math.random() * 0.8 * (canvas.clientWidth)) + "px";
+apple.style.top = Math.floor(Math.random() * 0.8 * (canvas.clientHeight)) + "px";
+snake.style.left = xPos + "px";
+snake.style.top = yPos + "px";
+let storedHighscore = parseInt(localStorage.getItem('myNumber'));
+if(Number.isNaN(storedHighscore)) {
+	localStorage.setItem("myNumber", 0);
+}
+highScoreText.innerHTML = `HighScore: ${storedHighscore}`;
+for(let i = 0; i < (30 / speed) * 3; i++) {
+	positions.push({x: parseFloat(snake.style.left) + speed * i, y: parseFloat(snake.style.top)});
+}
+for(let i = 0; i < snakeSquares.length; i++) {
+	snakeSquares[i].style.left = positions[(i+1) * (30 / speed) - 1].x + "px";
+	snakeSquares[i].style.top = positions[(i+1) * (30 / speed) - 1].y + "px";
+}
+let game = setInterval(draw, intervalTime);
 
-setInterval(draw, 200);
-let fruit = {x: Math.floor(Math.random() * 20) * square, y: Math.floor(Math.random() * 20) * square};
-snake[0] = {x: square * 5, y: canvas.height / 2};
-for(let i = 1; i < 3; i++){
-	snake[i] = {x: snake[0].x + square * i, y: canvas.height / 2};
-}
-apple.src = "icons8-red-apple-48.png";
-function restart() {
-	if (Score > highScore) highScore = Score
-	gameEnd.style.top = "";
-	gameEnd.style.left = "";
-	overGame.style.top = "";
-	overGame.style.left = "";
-	snake = [];
-	direction = "nodirection";
-	Score = 0;
-	snake[0] = {x: square * 5, y: canvas.height / 2};
-	for(let i = 1; i<3; i++){
-		snake[i] = {x: snake[0].x + square * i, y: canvas.height / 2};
-	}
-	fruit = {
-		x: Math.floor(Math.random() * 20) * square, 
-		y: Math.floor(Math.random() * 20) * square
-	};
-}
+// KEYBOARD AND SWIPE EVENT LISTENERS 
 document.addEventListener("keydown", function(){
 	if(event.key === "ArrowLeft") {
-		if(direction !== "right" && !draw()) direction = "left";
+		if(direction !== "right") direction = "left";
+
 	}
 	if(event.key === "ArrowUp") {
-		if(direction !== "down" && !draw()) direction = "up";
+		if(direction !== "down") 
+			direction = "up";
 	}
 	if(event.key === "ArrowDown") {
-		if(direction !== "up" && !draw()) direction = "down";
+		if(direction !== "up") direction = "down";
 	}
 	if(event.key === "ArrowRight") {
-		if(direction!== "left" && !draw()) if(direction !== "nodirection") direction = "right";
+		if(direction!== "left") 
+			if(direction !== "nodirection") direction = "right";
 	}
 })
+// SWIPE EVENTS
+document.addEventListener('touchstart', handleTouchStart, false);        
+document.addEventListener('touchmove', handleTouchMove, false);
 
+var xDown = null;                                                        
+var yDown = null;
+
+function getTouches(evt) {
+  return evt.touches ||             // browser API
+         evt.originalEvent.touches; // jQuery
+}                                                     
+                                                                         
+function handleTouchStart(evt) {
+    const firstTouch = getTouches(evt)[0];                                      
+    xDown = firstTouch.clientX;                                      
+    yDown = firstTouch.clientY;                                      
+};                                                
+                                                                         
+function handleTouchMove(evt) {
+    if ( ! xDown || ! yDown ) {
+        return;
+    }
+
+    var xUp = evt.touches[0].clientX;                                    
+    var yUp = evt.touches[0].clientY;
+
+    var xDiff = xDown - xUp;
+    var yDiff = yDown - yUp;
+                                                                         
+    if ( Math.abs( xDiff ) > Math.abs( yDiff ) ) {/*most significant*/
+        if ( xDiff > 0 ) {
+		if(direction !== "right") direction = "left";
+        } else {
+		if(direction!== "left") 
+			if(direction !== "nodirection") direction = "right";
+        }                       
+    } else {
+        if ( yDiff > 0 ) {
+		if(direction !== "down") 
+			direction = "up";
+        } else { 
+		if(direction !== "up") direction = "down";
+        }                                                                 
+    }
+    /* reset values */
+    xDown = null;
+    yDown = null;                                             
+};
+
+// PAUSE EVENT 
+document.addEventListener("keydown", function(event) {
+	if(event.key === "Esc" || event.key === "Escape") {
+		if(!pause) {
+			clearInterval(game);
+			pause = true;
+			document.getElementById("pause").style.display = "flex";
+		}
+		else {
+			game = setInterval(draw, intervalTime);
+			pause = false;
+			document.getElementById("pause").style.display = "none";
+		}
+	}
+});
 // DRAWING FUNCTION
 function draw(){
-	ctx.clearRect(0, 0, canvas.width, canvas.height);
-	theScore.innerText = `Score: ${Score}`;
-	document.getElementById("highScore").innerHTML = `Highscore: ${highScore}`;
-	ctx.drawImage(apple, fruit.x, fruit.y, square, square);
-	ctx.fillStyle = "black";
-	ctx.fillRect(snake[0].x, snake[0].y, square, square);
-	ctx.fillStyle = "brown";
-	for(let i = 1; i < snake.length; i++){
-		ctx.fillRect(snake[i].x, snake[i].y, square, square);
+	if(direction === "left") {
+		// MOVING THE HEAD
+		xPos -= speed;
+		snake.style.left = xPos + "px";
+		if(parseFloat(snakeSquares[2].style.left) < 0) {
+			xPos = canvas.clientWidth;
+		}
 	}
-	let snakeX = snake[0].x;
-	let snakeY = snake[0].y;
-	if(direction === "left") snakeX -= square;
-	else if(direction === "up") snakeY -= square;
-	else if(direction === "down") snakeY += square;
-	else if(direction === "right") snakeX += square;
-	let newHead = {x: snakeX, y: snakeY};
-	if(direction !== "nodirection"){
-		if(snake[0].x !== fruit.x || snake[0].y !== fruit.y) 
-			snake.pop();
-		else{
-			fruit.x = Math.floor(Math.random() * 20) * square;
-			fruit.y = Math.floor(Math.random() * 17) * square;
-			Score++;
+	else if(direction === "up") {
+		// MOVING THE HEAD
+		yPos -= speed;
+		snake.style.top = yPos + "px";
+		if(parseFloat(snakeSquares[2].style.top) < 0) {
+			yPos = canvas.clientHeight;
+		}
+	}
+	else if(direction === "down") {
+		// MOVING THE HEAD
+		yPos += speed;
+		snake.style.top = yPos + "px";
+		if(parseFloat(snakeSquares[2].style.top) > canvas.clientHeight) {
+			yPos = 0;
+		}
+		
+	}
+	else if(direction === "right") {
+		//MOVING THE HEAD
+		xPos += speed;
+		snake.style.left = xPos + "px";
+		if(parseFloat(snakeSquares[2].style.left) > canvas.clientWidth) {
+			xPos = 0;
+		}
+		
+	}
+	
+	
+
+
+
+	// INCREASE WHEN GETTING THE APPLE
+	if(collision(parseFloat(snake.style.left), parseFloat(snake.style.top), parseFloat(apple.style.left), parseFloat(apple.style.top), 30)) {
+		score++;
+		scoreText.innerHTML = `Score: ${score}`;
+		apple.style.left = Math.floor(Math.random() * 0.8 * (canvas.clientWidth)) + "px";
+		apple.style.top = Math.floor(Math.random() * 0.8 * (canvas.clientHeight)) + "px";
+		const newSquare = document.createElement("div");
+		newSquare.classList.add("squares");
+		canvas.appendChild(newSquare);
+		newSquare.style.left = snakeSquares[snakeSquares.length - 2].style.left;
+		newSquare.style.top = snakeSquares[snakeSquares.length - 2].style.top;
+		document.getElementById("audio").play();
+		storedHighscore = parseInt(localStorage.getItem('myNumber'));
+		if(score > storedHighscore) {
+			storedHighscore = score;
+		}
+		localStorage.setItem("myNumber", storedHighscore);
+		highScoreText.innerHTML = `HighScore: ${storedHighscore}`;
+		clearInterval(game);
+		intervalTime-= 0.01;
+		game = setInterval(draw, intervalTime);
+
+		
+	}
+	if(direction != "nodirection") {
+		positions.unshift({x: parseFloat(snake.style.left), y: parseFloat(snake.style.top)});
+		for(let i = 0; i < snakeSquares.length; i++) {
+			snakeSquares[i].style.left = positions[(i+1) * (30 / speed) - 1].x + "px";
+			snakeSquares[i].style.top = positions[(i+1) * (30 / speed) - 1].y + "px";
+		}
+	}
+	// MAKING COLLISION WITH ITSELF
+	for(let i = 2; i < snakeSquares.length; i++) {
+		if(collision(parseFloat(snake.style.left), parseFloat(snake.style.top), parseFloat(snakeSquares[i].style.left), parseFloat(snakeSquares[i].style.top), 30)) {
+			clearInterval(game);
+			window.location.href = 'menu.html';
 
 		}
-		snake.unshift(newHead);
 	}
-
-	if(collision(snake, snakeX, snakeY) || snakeX < 0 || snakeX >= canvas.width|| snakeY < 0 || snakeY >= canvas.height) {
-		direction = "nodirection";
-		gameEnd.style.top = "calc(50% - 350px / 2)";
-		gameEnd.style.left = "calc(50% - 300px / 2)";
-		overGame.style.top = "calc(40% - 150px / 2)";
-		overGame.style.left = "calc(50% - 300px / 2)";
-		theRetry.addEventListener("click", restart);
-	}
+	
 }
-function collision(theSnake, x, y){
-	for(let i = 1; i<theSnake.length; i++){
-		if(x === snake[i].x && y === snake[i].y) return true;
+
+function collision(snakeX, snakeY, appleX, appleY, size) {
+	if(snakeX > appleX && snakeX < appleX + size) {
+		if(snakeY > appleY && snakeY < appleY + size) return true;
+	}
+	if(snakeX + size > appleX && snakeX + size < appleX + size) {
+		if(snakeY + size > appleY && snakeY + size < appleY + size) return true;
 	}
 	return false;
 }
-
 
 
 
